8c5d8af92a879417cc9089ed34535ee1edf08f3a
Abdulkodir Burganov <feonorabd2000@gmail.com>
2024-02-25 22:07:07 +0300

    Added api to add and get classes

--- pages/api/classes.js
+++ pages/api/classes.js
@@ -1,18 +1,44 @@
-export default function handler(req, res) {
-    const {body, method} = req;
+import postgresql from "@/postgresql";
+
+export default async function handler(req, res) {
+    const {method} = req;
 
 
     switch (method) {
         case 'GET':
-
+            await getClasses(res);
+            return;
+        case 'POST':
+            await createClass(req, res);
         default:
             res.status(405).end('Method is not allowed');
-
     }
 
     res.status(200).json({ message: 'Hello from Next.js!' })
 }
 
-const getClasses = () => {
+const getClasses = async (res) => {
+    const {SchoolClass, Student} = await postgresql.getInstance();
+    const classes = await SchoolClass.findAll({include: Student});
 
+    const output = classes.map(({dataValues, Students}) => ({...dataValues, ...Students.map(({dataValues}) => dataValues)}));
+
+    res.status(200).json({output});
+}
+
+const createClass = async (req, res) => {
+    const {body} = req;
+
+    if (!body.className || !body.students?.length) {
+        res.status(400).end('Required className and students');
+        return;
+    }
+
+    const {Student, SchoolClass} = await postgresql.getInstance();
+
+    const schoolClass = await SchoolClass.create({name: body.className});
+
+    await Promise.all(body.students.map(student => Student.create({fullName: student, SchoolClassId: schoolClass.id})));
+
+    res.status(200).end();
 }
